e886e8171a63942d8812ab92eb90ad4cb2c4358b
Raj Magesh Gauthaman <git@raj-magesh.org>
2026-06-24 17:59:34 -0400

    feat: initial Python API for ripple_control

--- src/seeg_utilities/ripple_control/+ripple_control/send_trigger.m
+++ src/seeg_utilities/ripple_control/+ripple_control/send_trigger.m
@@ -5,7 +5,7 @@ function send_trigger(trigger_value)
 
     try
         xippmex('digout', 5, double(trigger_value));
-        fprintf('Successfully sent trigger %s to parallel port!\n', num2str(trigger_value));
+        fprintf('Sent trigger %s to parallel port\n', num2str(trigger_value));
     catch
         warning('Failed to send trigger %s to parallel port\n', num2str(trigger_value));
     end
